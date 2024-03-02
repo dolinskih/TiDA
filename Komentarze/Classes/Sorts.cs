3960630d712008a26f452d7dc88140173401461e
dolinskih <047hubert@gmail.com>
2024-03-02 19:21:03 +0100

    Przykładowy projekt z komentarzami

---
+++ Komentarze/Classes/Sorts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komentarze.Classes
+{
+    /// <summary>
+    /// Klasa z metodami sortującymi tablice
+    /// </summary>
+    internal class Sorts
+    {
+        /// <summary>
+        /// QuickSort
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="leftIndex"></param>
+        /// <param name="rightIndex"></param>
+        /// <returns>Posortowana tablica int[]</returns>
+        public int[] QuickSortArray(int[] array, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                QuickSortArray(array, leftIndex, j);
+
+            if (i < rightIndex)
+                QuickSortArray(array, i, rightIndex);
+
+            return array;
+        }
+    }
+}
---
+++ Komentarze/Program.cs
@@ -0,0 +1,16 @@
+﻿using Komentarze.Classes;
+
+///Stworzenie testowej tablicy intów
+int[] testArray = [1, 7, 3, 9, 6];
+
+///Inicjalizacja klasy Sorts
+Sorts sorts = new Sorts();
+
+///Zdefiniowanie posortowanej tablicy intów z użyciem meotdy QuickSortArray z klasy Sorts
+int[] quickSortedTestArray = sorts.QuickSortArray(testArray, 0, testArray.Length-1);
+
+///Wypisanie posortowanej zawartości tablicy
+foreach (int i in quickSortedTestArray)
+{
+    Console.Write($"{i} ");
+}
